2b5d41c115e9529962492267bff38f301b3f2a40
Andy Waterman <35544033+A-Waterman@users.noreply.github.com>
2018-09-14 13:29:01 -0600

    dual covariance estimators
    Ported dual covariance estimator code from notebook to script.

--- experiments.py
+++ experiments.py
@@ -35,6 +35,25 @@ def experiment(exp):
     print("Valid fp: {}".format(exp.false_positives(X_valid, y_valid)))
 
 
+def dual_experiment(exp):
+    np.random.seed(0)
+
+    safe_dataset, train_dataset, test_dataset = transform_datasets(full_data)
+
+    X_safe, y_safe = split_data(safe_dataset)
+    X_train, y_train = split_data(train_dataset)
+    X_test, y_test = split_data(test_dataset)
+
+    X_train, y_train, X_valid, y_valid = train_valid_split(X_train, y_train)
+
+    exp.fit_cov(X_safe)
+    exp.fit(X_train, y_train)
+    
+    print("Safe fp: {}".format(exp.false_positives(X_safe, y_safe)))
+    print("Valid MCC: {:.3f}".format(exp.score(X_valid, y_valid)))
+    print("Valid fp: {}".format(exp.false_positives(X_valid, y_valid)))
+
+
 def GMM_experiment(n_components, covariance_type='full', random_state=None, neighbors=10, weights='uniform'):
     gmm = GaussianMixture(n_components=n_components, covariance_type='full', random_state=random_state)
     gmm_exp = cov_detector(gmm, neighbors=neighbors, weights=weights)
