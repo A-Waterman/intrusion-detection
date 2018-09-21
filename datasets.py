149c3fcc750617944cd7ac181780780172cf5bc7
Andy Waterman <35544033+A-Waterman@users.noreply.github.com>
2018-09-21 15:01:30 -0600

    First training dataset
    Code to download and label the first training dataset.

--- datasets.py
+++ datasets.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from pathlib import Path
+from src.util import label_attack
 
 
 def normalize(base, dataset):
@@ -8,11 +10,29 @@ def normalize(base, dataset):
     return dataset
 
 
-def main():
-    training_0 = pd.read_csv("https://www.batadal.net/data/BATADAL_dataset03.csv")
-    training_1 = pd.read_csv("https://www.batadal.net/data/BATADAL_dataset04.csv")
+def label_attacks(dataset, type=""):
+    Attacks = ['ATT_FLAG',
+                'ATT_T1', 'ATT_T2', 'ATT_T3', 'ATT_T4', 'ATT_T5', 'ATT_T6', 'ATT_T7',
+                'ATT_PU1', 'ATT_PU2', 'ATT_PU3', 'ATT_PU4', 'ATT_PU5', 'ATT_PU6', 'ATT_PU7',
+                'ATT_PU8', 'ATT_PU9', 'ATT_PU10', 'ATT_PU11', 'ATT_V2']
 
-    training_1 = normalize(training_0, training_1)
+    for key in Attacks:
+        dataset[key] = 0
+
+    if type == "safe":
+        return dataset
+    else:
+        print("Error: type must be one of 'safe', 'train', or 'test'")
+        return None
+
+
+def main():
+    if Path("datasets/train_0.csv").exists():
+        training_0 = pd.read_csv("datasets/train_0.csv")
+    else:
+        training_0 = pd.read_csv("https://www.batadal.net/data/BATADAL_dataset03.csv")
+        training_0 = label_attacks(training_0, "safe")
+        training_0.to_csv('datasets/train_0.csv', index=False)
 
 
 if __name__ == '__main__':
