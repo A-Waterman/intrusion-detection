98bba20dbb48e632d351c0bb03b8dd169d5eddb6
Andy Waterman <35544033+A-Waterman@users.noreply.github.com>
2018-09-21 14:47:56 -0600

    Normalize training dataset
    Code to normalize training dataset. Second training dataset has superfluous space.

--- datasets.py
+++ datasets.py
@@ -2,13 +2,17 @@ import pandas as pd
 import numpy as np
 
 
+def normalize(base, dataset):
+    for index in range(len(base.columns)):
+        dataset.rename(columns={dataset.columns[index]: base.columns[index]}, inplace=True)
+    return dataset
+
+
 def main():
     training_0 = pd.read_csv("https://www.batadal.net/data/BATADAL_dataset03.csv")
     training_1 = pd.read_csv("https://www.batadal.net/data/BATADAL_dataset04.csv")
 
-    # normalize column names (e.g. ' ATT_FLAG' to 'ATT_FLAG')
-    for index in range(len(training_0.columns)):
-        training_1.rename(columns={training_1.columns[index]: training_0.columns[index]}, inplace=True)
+    training_1 = normalize(training_0, training_1)
 
 
 if __name__ == '__main__':
