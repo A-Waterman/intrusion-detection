8ca40c4593a285015278e1c3a72677f746d476f6
Andy Waterman <35544033+A-Waterman@users.noreply.github.com>
2018-09-21 14:43:12 -0600

    Download and normalize training
    Code to download and normalize both of the training datasets.

--- datasets.py
+++ datasets.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import numpy as np
-import urllib2
+
 
 def main():
-    pass
+    training_0 = pd.read_csv("https://www.batadal.net/data/BATADAL_dataset03.csv")
+    training_1 = pd.read_csv("https://www.batadal.net/data/BATADAL_dataset04.csv")
+
+    # normalize column names (e.g. ' ATT_FLAG' to 'ATT_FLAG')
+    for index in range(len(training_0.columns)):
+        training_1.rename(columns={training_1.columns[index]: training_0.columns[index]}, inplace=True)
+
 
 if __name__ == '__main__':
     main()
