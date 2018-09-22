4200b0f3a6f6f5d2b9663cc06701e01a926e5c07
Andy Waterman <35544033+A-Waterman@users.noreply.github.com>
2018-09-22 10:35:08 -0600

    refactored attack labels
    Code to label attacks and attacked components.

--- src/util.py
+++ src/util.py
@@ -46,11 +46,36 @@ def normalize(base, dataset):
     for index in range(len(base.columns)):
         dataset.rename(columns={dataset.columns[index]: base.columns[index]}, inplace=True)
 
-
 def label_attack(dataset, index_start, index_end, components):
     dataset.loc[index_start:index_end, 'ATT_FLAG'] = 1
     for component in components:
         dataset.loc[index_start:index_end, component] = 1
+
+def label_attacks(dataset, type=""):
+    Attacks = ['ATT_FLAG',
+                'ATT_T1', 'ATT_T2', 'ATT_T3', 'ATT_T4', 'ATT_T5', 'ATT_T6', 'ATT_T7',
+                'ATT_PU1', 'ATT_PU2', 'ATT_PU3', 'ATT_PU4', 'ATT_PU5', 'ATT_PU6', 'ATT_PU7',
+                'ATT_PU8', 'ATT_PU9', 'ATT_PU10', 'ATT_PU11', 'ATT_V2']
+
+    for key in Attacks:
+        dataset[key] = 0
+
+    if type == "train":
+        label_attack(dataset, 1727, 1776, ['ATT_T7'])
+        label_attack(dataset, 2027, 2050, ['ATT_T7', 'ATT_PU10', 'ATT_PU11'])
+        label_attack(dataset, 2337, 2396, ['ATT_T1'])
+        label_attack(dataset, 2827, 2920, ['ATT_T1', 'ATT_PU1', 'ATT_PU2']) # J269
+        label_attack(dataset, 3497, 3556, ['ATT_PU7'])
+        label_attack(dataset, 3727, 3820, ['ATT_T4', 'ATT_PU7'])
+        label_attack(dataset, 3927, 4036, ['ATT_T1', 'ATT_T4', 'ATT_PU1', 'ATT_PU2', 'ATT_PU7'])
+    elif type == "test":
+        label_attack(dataset, 297, 366, ['ATT_T3', 'ATT_PU4', 'ATT_PU5'])
+        label_attack(dataset, 632, 696, ['ATT_T2', 'ATT_V2'])
+        label_attack(dataset, 867, 897, ['ATT_PU3'])
+        label_attack(dataset, 939, 969, ['ATT_PU3'])
+        label_attack(dataset, 1229, 1328, ['ATT_T2', 'ATT_V2']) # J14, J422
+        label_attack(dataset, 1574, 1653, ['ATT_T7', 'ATT_PU10', 'ATT_PU11']) # J14, J422
+        label_attack(dataset, 1940, 1969, ['ATT_T4'])
 
 def label_save_datasets():
     # load the downloaded datasets
